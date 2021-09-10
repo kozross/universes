10838aebe7a34d11a3ff354b07f73bda0a62204a
Koz Ross <koz.ross@retro-freedom.nz>
2021-09-10 17:04:03 +1200

    These instance of Abelian, more docs

--- src/Data/Semigroup/Abelian.hs
+++ src/Data/Semigroup/Abelian.hs
@@ -6,19 +6,39 @@ module Data.Semigroup.Abelian
   )
 where
 
+import Control.Applicative (Const (Const))
 import Data.Functor.Identity (Identity (Identity))
 import Data.Int (Int16, Int32, Int64, Int8)
 import Data.IntSet (IntSet)
 import qualified Data.IntSet as ISet
 import Data.Kind (Type)
-import Data.Semigroup (Min (Min), Sum (Sum))
+import Data.Semigroup (Max (Max), Min (Min), Sum (Sum))
 import Data.Set (Set)
 import qualified Data.Set as Set
+import Data.These (These (That, These, This))
 import Numeric.Natural (Natural)
 
--- | @since 1.0
+-- | = Laws
+--
+-- 1. @x <> y = y <> x@ (commutativity of '<>')
+-- 2. There exists @x, y@ such that @'not' '.' 'null' '$' 'factor' x y@
+--    (non-triviality).
+-- 3. If @'factor' x y = z@, then @'all' ((x '==') . (y '<>')) z@ and
+--    @'all' ((x '==') . ('<>' y)) z@ (recombination).
+-- 4. If @'factor' y x = factYX@ and @'factor' z y = factZY'@, then @'factor' z x
+--    = ???@
+--
+-- If @s@ is also a 'Monoid', then the following law also holds:
+--
+-- 5. @'not' . 'null' '$' 'factor' x x@ (????)
+--
+-- @since 1.0
 class (Eq s, Semigroup s) => Abelian (s :: Type) where
-  -- | @since 1.0
+  -- | @'factor' x y@ produces a list of all @z@ such that @y '<>' z = z '<>' y
+  -- = x@. There may be none, finitely many, or infinitely many, for any given
+  -- choices of @x@ and @y@.
+  --
+  -- @since 1.0
   factor :: s -> s -> [s]
 
 -- | @since 1.0
@@ -90,12 +110,21 @@ instance (Ord a) => Abelian (Min a) where
       GT -> []
 
 -- | @since 1.0
+instance (Ord a) => Abelian (Max a) where
+  {-# INLINEABLE factor #-}
+  Max x `factor` Max y =
+    Max <$> case compare x y of
+      LT -> []
+      EQ -> [x]
+      GT -> [x]
+
+-- | @since 1.0
 instance (Abelian a) => Abelian (Maybe a) where
   {-# INLINEABLE factor #-}
   x `factor` y = case (x, y) of
     (Nothing, Nothing) -> [Nothing]
     (Nothing, Just _) -> []
-    (Just x', Nothing) -> [Just x']
+    (Just x', Nothing) -> Just x' : (Just <$> factor x' x')
     (Just x', Just y') ->
       let factors = Just <$> factor x' y'
        in if x' == y'
@@ -110,4 +139,28 @@ instance (Abelian a) => Abelian (Identity a) where
 -- | @since 1.0
 instance (Abelian a, Abelian b) => Abelian (a, b) where
   {-# INLINEABLE factor #-}
-  (x, x') `factor` (y, y') = [(z, z') | z <- x `factor` y, z' <- x' `factor` y']
+  (x, x') `factor` (y, y') =
+    [(z, z') | z <- x `factor` y, z' <- x' `factor` y']
+
+-- | @since 1.0
+instance (Abelian b) => Abelian (Const b a) where
+  {-# INLINEABLE factor #-}
+  Const x `factor` Const y = Const <$> factor x y
+
+-- | @since 1.0
+instance (Abelian a, Abelian b) => Abelian (These a b) where
+  This x `factor` This y = This <$> factor x y
+  That x `factor` That y = That <$> factor x y
+  These x y `factor` These x' y' =
+    These <$> factor x x' <*> factor y y'
+  These x y `factor` This x' =
+    let factors = (`These` y) <$> factor x x'
+     in if x == x'
+          then That y : factors
+          else factors
+  These x y `factor` That y' =
+    let factors = These x <$> factor y y'
+     in if y == y'
+          then This x : factors
+          else factors
+  _ `factor` _ = []
